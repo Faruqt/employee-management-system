72a3278736a841c8391658f3c182b3f49efb824e
Faruqt <faruqabdulsalam@yahoo.com>
2024-11-26 12:50:10 +0000

    chore: add script to start run migration before starting application

---
+++ startup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Print a message
+echo "Checking and applying database migrations..."
+
+# Run database migrations
+bundle exec rails db:migrate || echo "Database migrations applied."
+
+# Start the Rails server
+./bin/rails server
