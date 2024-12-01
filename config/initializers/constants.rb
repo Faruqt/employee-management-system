a7bb67411312929ff8263d63bf5e64b63cfd992a
Faruqt <faruqabdulsalam@yahoo.com>
2024-12-01 01:23:57 +0000

    chore: create constants initializer

---
+++ config/initializers/constants.rb
@@ -0,0 +1,10 @@
+# config/initializers/constants.rb
+
+module Constants
+  # Date formats
+  DATE_FORMAT = "%Y-%m-%d".freeze
+  DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S".freeze
+
+  # Pagination settings
+  DEFAULT_PER_PAGE = 20
+end
