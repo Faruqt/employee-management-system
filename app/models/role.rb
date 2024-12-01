30052c75025fc44d7de16c41e7c89d6ae31af212
Faruqt <faruqabdulsalam@yahoo.com>
2024-12-01 19:54:35 +0000

    update: add role public attributes method

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,2 +1,12 @@
 class Role < ApplicationRecord
+    def public_attributes
+        {
+            id: id,
+            name: name,
+            symbol: symbol,
+            area_id: area_id,
+            created_at: created_at.strftime(Constants::DATETIME_FORMAT),
+            updated_at: updated_at.strftime(Constants::DATETIME_FORMAT)
+        }
+    end
 end
