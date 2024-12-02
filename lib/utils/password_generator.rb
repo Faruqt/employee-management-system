aadb7c17ead5bb8c6f6d4161287a7f9b17cf8a30
Faruqt <faruqabdulsalam@yahoo.com>
2024-12-02 00:55:21 +0000

    update: add string to generated password

--- lib/utils/password_generator.rb
+++ lib/utils/password_generator.rb
@@ -3,8 +3,12 @@
 module Utils
   module PasswordGenerator
     def self.generate_password(length)
-      # Generate a random password of the specified length
+      # Generate a random password of the specified length with numbers
       password_string = rand(10**(length - 1)..(10**length - 1)).to_s
+
+      # Add a random lowercase character to the password
+      password_string += (('a'..'z').to_a.sample)
+
       password_string
     end
   end
