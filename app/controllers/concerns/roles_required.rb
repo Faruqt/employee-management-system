f74fcba1d29bc56882b499a59b96094c5dbf29e1
Faruqt <faruqabdulsalam@yahoo.com>
2024-12-08 02:45:44 +0000

    feat: add roles required concern

---
+++ app/controllers/concerns/roles_required.rb
@@ -0,0 +1,23 @@
+module RolesRequired
+  extend ActiveSupport::Concern
+
+  private
+
+  def roles_required(allowed_roles)
+
+    # Check if current_user is present
+    if @current_user.nil?
+      render json: { message: "Unauthorized access" }, status: :unauthorized
+      return
+    end
+
+    # Find the admin by email
+    admin = Admin.find_by(email: @current_user["email"])
+
+    # Check if the admin exists and if they have the required role
+    unless admin && allowed_roles.any? { |role| admin.send("is_#{role}") }
+      Rails.logger.error("Unauthorized access by user: #{@current_user["email"]}")
+      render json: { message: "You are not authorized to perform this action" }, status: :unauthorized
+    end
+  end
+end
