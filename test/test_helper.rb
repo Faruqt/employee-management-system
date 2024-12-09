1be926b28c2932521fcafe83742c121c1868cd77
Faruqt <faruqabdulsalam@yahoo.com>
2024-12-09 00:40:43 +0000

    test: improve stub mocking of authentication methods

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -32,8 +32,77 @@ module ActiveSupport
       # Stub the logout method to return a successful response
       @mock_cognito_service.stubs(:revoke_token).returns(true)
 
+      # Stub the set_new_password method to return a successful response
+      @mock_cognito_service.stubs(:set_new_password).returns(true)
+
+      #stub the verify email method to return a successful response
+      @mock_cognito_service.stubs(:verify_email).returns(true)
+
+      # stub the request_password_reset method to return a successful response
+      @mock_cognito_service.stubs(:request_password_reset).returns(true)
+
+      # stub the reset password method to return a successful response
+      @mock_cognito_service.stubs(:reset_password).returns(true)
+
+      # stub the admin set password method to return a successful response
+      @mock_cognito_service.stubs(:admin_set_password).returns(true)
+
+      # stub the change password method to return a successful response
+      @mock_cognito_service.stubs(:change_password).returns(true)
+
       # Mock the CognitoService initialization to return the mock object
       CognitoService.stubs(:new).returns(@mock_cognito_service)
+
+    end
+
+    def setup_cognito_mock_for_authentication(email)
+      # Mock the Cognito Identity Provider client
+      @mock_cognito_client = mock("Aws::CognitoIdentityProvider::Client")
+
+      # Stub the get_user method to simulate a successful response
+      @mock_cognito_client.stubs(:get_user).returns(
+        Aws::CognitoIdentityProvider::Types::GetUserResponse.new(
+          username: "mock_username",
+          user_attributes: [
+            Aws::CognitoIdentityProvider::Types::AttributeType.new(name: "email", value: email)
+          ]
+        )
+      )
+
+      # Inject the mock into your app's AWS client
+      Aws::CognitoIdentityProvider::Client.stubs(:new).returns(@mock_cognito_client)
+    end
+
+    def setup_test_data
+      # Create test data
+      @org = Organization.create!(name: "Organization One", address: "Address One")
+      @branch = Branch.create!(name: "Branch One", address: "Branch Address One", organization: @org)
+      @area = Area.create!(name: "Area One", color: "Color One")
+      @branch.areas << @area
+      @manager1 = Admin.create!(first_name: "John", last_name: "Doe", email: "test_manager_one@gmail.co", telephone: "123456789", is_manager: true, branch: @branch, area: @area)
+
+      @director1 = Admin.create!(first_name: "Jane", last_name: "Doe", email: "test_director_one@gmail.co", telephone: "123456789", is_director: true, branch: @branch)
+      @super_admin = Admin.create!(first_name: "Super", last_name: "Admin", email: "test_super_one@gmail.co", telephone: "123456789", is_super_admin: true)
+      @employee1 = Employee.create!(first_name: "John", last_name: "Doe", email: "test_employee_one@gmail.co", telephone: "123456789", branch: @branch, area: @area, contract_code: "123456", tax_code: "654321", date_of_birth: "1990-01-01", contract_start_date: "2021-01-01", contract_end_date: "2022-01-01")
+    end
+
+    def setup_authenticated_session(user)
+      # Create a session for the user by simulating a successful login
+      @mock_cognito_service.stubs(:authenticate).returns(
+        Aws::CognitoIdentityProvider::Types::InitiateAuthResponse.new(
+          authentication_result: Aws::CognitoIdentityProvider::Types::AuthenticationResultType.new(
+            access_token: "mock_access_token",
+            refresh_token: "mock_refresh_token"
+          )
+        )
+      )
+
+      post "/auth/login", params: { email: user.email, password: "password" }
+      assert_response :ok
+      response_data = ::JSON.parse(response.body)
+
+      # Return the user and the tokens
+      { user: user, access_token: response_data["access_token"], refresh_token: response_data["refresh_token"] }
     end
 
     # Add more helper methods to be used by all tests here...
