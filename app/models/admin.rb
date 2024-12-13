a3b0a121b612d9dc0cab76300c9bfff151527063
Faruqt <faruqabdulsalam@yahoo.com>
2024-12-13 04:32:32 +0000

    feat: create admin type constant

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,7 +1,17 @@
 class Admin < ApplicationRecord
+    # Constants for admin types
+    ADMIN_TYPES = {
+        manager: 'manager',
+        director: 'director',
+        super_admin: 'super_admin',
+    }.freeze
+
     # Validations
     validates :first_name, presence: true
     validates :email, presence: true, uniqueness: true
+    
+    # Validation for admin_type only if it's being set
+    validate :validate_admin_type, if: :admin_type?
 
     # Callbacks
     before_validation :downcase_email
@@ -13,10 +23,8 @@ class Admin < ApplicationRecord
             last_name: last_name,
             email: email,
             telephone: telephone,
-            is_manager: is_manager,
-            is_director: is_director,
+            admin_type: admin_type,
             is_deleted: is_deleted,
-            is_super_admin: is_super_admin,
             area: area&.public_attributes,
             branch: branch&.public_attributes,
             created_at: created_at.strftime(Constants::DATETIME_FORMAT),
@@ -24,10 +32,25 @@ class Admin < ApplicationRecord
         }
     end
 
+    def admin_type_name
+        ADMIN_TYPES.key(admin_type).to_s.capitalize
+    end
+
+    def self.admin_types
+        ADMIN_TYPES
+    end
+
+
     private
 
     def downcase_email
         self.email = email.downcase if email.present?
+    end
+
+    def validate_admin_type
+        unless ADMIN_TYPES.values.include?(admin_type)
+            errors.add(:admin_type, "is not included in the list")
+        end
     end
 
     # One to Many relationship with Branches
